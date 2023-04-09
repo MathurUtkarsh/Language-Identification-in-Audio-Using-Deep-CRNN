a7744c0bb18b42d2ded857b324d4a6796ef108ed
Utkarsh Mathur <78642104+MathurUtkarsh@users.noreply.github.com>
2023-04-09 19:14:46 +0530

    Update clientApp.py

--- clientApp.py
+++ clientApp.py
@@ -3,7 +3,7 @@ import os
 from flask_cors import CORS, cross_origin
 
 from kerasa.predict2 import predict
-from com_in_ineuron_ai_utils.utils import decodeSound
+from my_utility.utils import decodeSound
 
 os.putenv('LANG', 'en_US.UTF-8')
 os.putenv('LC_ALL', 'en_US.UTF-8')
