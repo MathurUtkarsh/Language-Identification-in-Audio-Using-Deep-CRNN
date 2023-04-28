85877379c54ea47813620d6d5ba94b3feb7f6900
MathurUtkarsh <utkarshmathur05@gmail.com>
2023-04-28 18:01:10 +0530

    Update clientApp.py

--- clientApp.py
+++ clientApp.py
@@ -1,9 +1,8 @@
 from flask import Flask, request, jsonify,render_template
 import os
 from flask_cors import CORS, cross_origin
-
 from kerasa.predict2 import predict
-from my_utility.utils import decodeSound
+from my_utilities.decode import decodeSound
 
 os.putenv('LANG', 'en_US.UTF-8')
 os.putenv('LC_ALL', 'en_US.UTF-8')
@@ -22,10 +21,11 @@ def predictRoute():
     image = request.json['sound']
     decodeSound(image, "inputSound.wav")
     result = predict()
-    return jsonify({"Result" : result})
+    return jsonify({"Result": result})
 
 
-#port = int(os.getenv("PORT"))
+# port = int(os.getenv("PORT"))
+
 if __name__ == "__main__":
-    #app.run(host='0.0.0.0', port=port)
+
     app.run(host='0.0.0.0', port=5000, debug=True)
